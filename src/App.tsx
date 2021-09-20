9cc2822e923fa9dceb4c290c077fc4d07387c314
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-09-20 12:14:13 -0300

    Added styled types

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
-import './App.css'
+import styled from 'styled-components'
 
 import UpdateMessage from './UpdateMessage'
 
+const ContainerApp = styled.div`
+    text-align: center;
+`
+
+const AppHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <ContainerApp>
+            <AppHeader>
                 Electron app with auto update!
                 <UpdateMessage />
-            </header>
-        </div>
+            </AppHeader>
+        </ContainerApp>
     )
 }
 
